d1cdfbb15bee7b5d529b735dd03bcaf9592e4042
Desmond Rawls <captaingrover@gmail.com>
2013-07-07 20:22:40 -0400

    add scraper with seed.rb to populate db via rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "#{Rails.root}/lib/scraper.rb"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+scraped_students = StudentScrape.scrape_students.compact
+
+  scraped_students.each do |s|
+    Student.create(:name => s.name, :twitter => s.twitter, :linkedin => s.linkedin, :github => s.github, :quote => s.quote, :bio => s.bio)
+  end
